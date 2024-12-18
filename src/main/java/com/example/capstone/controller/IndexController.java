ee4e3d2575ebd1efa7a3f0e4d04932bf546f65a6
Alagusoundarya Manivasagam <alagusoundarya1995@gmail.com>
2024-12-18 11:40:41 -0600

    Added more files and url to project

---
+++ src/main/java/com/example/capstone/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.example.capstone.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Slf4j
+@Controller
+public class IndexController {
+    @GetMapping(value={"/", "/index", "/index.html"})
+    public ModelAndView index() {
+        ModelAndView response = new ModelAndView();
+        response.setViewName("index");
+        return response;
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -0,0 +1,13 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: kumar
+  Date: 12/18/2024
+  Time: 11:27 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<jsp:include page="include/header.jsp"/>
+
+<h1>Hello</h1>
+
+<jsp:include page="include/footer.jsp"/>
